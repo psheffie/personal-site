4b56446e9de85d16b79aadf48a9e5575a6ecaf66
psheffie <psheffie@gmail.com>
2024-01-12 09:41:32 -0800

    Updated box dates

--- index.js
+++ index.js
@@ -18,3 +18,36 @@ elements.forEach(element => {
 
 
 
+const showDialog1 = () => {
+  document.getElementById('dialog1').classList.add('show')
+  document.getElementById('dialog2').classList.remove('show');
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  const body = document.body;
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollY}`;
+  window.scrollTo(0, 0);
+};
+
+const showDialog2 = () => {
+  document.getElementById('dialog2').classList.add('show')
+  document.getElementById('dialog1').classList.remove('show');
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  const body = document.body;
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollY}`;
+  window.scrollTo(0, 0);
+};
+
+
+const closeDialog = () => {
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+  document.getElementById('dialog1').classList.remove('show');
+  document.getElementById('dialog2').classList.remove('show');
+}
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+});
